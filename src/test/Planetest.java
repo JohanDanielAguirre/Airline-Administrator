149d5bc4f5ee05e0739d6e0ce14e326e34088425
[Mrzen] <1110290182@u.icesi.edu.co>
2023-04-30 13:40:15 -0500

    first version of test is ready

--- src/test/Planetest.java
+++ src/test/Planetest.java
@@ -3,116 +3,54 @@ import junit.framework.TestCase;
 import java.util.*;
 import model.*;
 import Exeptions.*;
-public class MaxHeaptest extends TestCase {
+public class Planetest extends TestCase {
 	
-    public void setUpStage1(){
-        Plane fly1= new Plane(/*numFLight*/ "MidgetAirlines",/*rows*/5,/*columns*/ 1,/*rowsfirstclass*/ 2);
-    }
-	public void setUpStage2(){
-        Plane fly2= new Plane(/*numFLight*/ "Drugmaxdealer",/*rows*/4,/*columns*/ 2,/*rowsfirstclass*/ 0);
-    }
-	public void setUpStage3(){
-        Plane fly3= new Plane(/*numFLight*/ "Viagrafly",/*rows*/4,/*columns*/ 2,/*rowsfirstclass*/ 4);
-    }
-	public void setUpStage4(){
-		Passenger s4p1 = new Passenger("olvir Eusebius",1110,1a,10,none,2023-04-01 08:30:00);
-		Passenger s4p2 = new Passenger("Cassian Rufina",290123,2b,100,none,2023-04-01 10:15:00);
-		Passenger s4p3 = new Passenger("Erkan Noriko",18200,1c,1000,none,2023-04-01 11:00:00);
-		Passenger s4p4 = new Passenger("Harpreet Aleksandra",610340,1d,250,none,2023-04-01 12:45:00);
-		Passenger s4p5 = new Passenger("Adalhaid Milda",950562,2a,2500,physical disability,2023-04-01 13:30:00);
-		Passenger s4p6 = new Passenger("Oskars Muthoni",396014,1e,25,mental disability,2023-04-01 15:00:00);
-		Passenger s4p7 = new Passenger("Anastasia Amahle",1130584,2c,365,none,2023-04-01 16:15:00);
-		Passenger s4p8 = new Passenger("Yasen Alaric",851944,2d,653,kid,2023-04-01 18:00:00);
-		Passenger s4p9 = new Passenger("Rozalija Raginaharjaz",1042984,2e,536,age,2023-04-01 19:45:00);
-		Passenger s4p10 = new Passenger("Yarik Komang",44100,1b,9564,pregnancy,2023-04-01 20:30:00);
-	}	
-	public void setUpStage5(){
-		Passenger s5p1 = new Passenger("olvir Eusebius",1110,1a,0,none,2023-04-01 08:30:00);
-		Passenger s5p2 = new Passenger("Cassian Rufina",290123,2b,0,none,2023-04-01 10:15:00);
-		Passenger s5p3 = new Passenger("Harpreet Aleksandra",18200,3c,0,none,2023-04-01 11:00:00);
-		Passenger s5p4 = new Passenger("Oskars Muthoni",610340,4d,0,none,2023-04-01 12:45:00);
-		Passenger s5p5 = new Passenger("Anastasia Amahle",950562,2a,0,none,2023-04-01 13:30:00);
-		Passenger s5p6 = new Passenger("Yasen Alaric",396014,1b,0,none,2023-04-01 15:00:00);
-		Passenger s5p7 = new Passenger("Rozalija Raginaharjaz",1130584,2c,0,none,2023-04-01 16:15:00);
-		Passenger s5p8 = new Passenger("Yarik Komang",851944,1d,0,none,2023-04-01 18:00:00);
-    }
-	public void setUpStage6(){
-		Passenger s6p1 = new Passenger("olvir Eusebius",1110,1a,350,physical disability,2023-04-01 08:30:00);
-		Passenger s6p2 = new Passenger("Cassian Rufina",290123,2b,125,mental disability,2023-04-01 10:15:00);
-		Passenger s6p3 = new Passenger("Harpreet Aleksandra",18200,3c,500,age,2023-04-01 11:00:00);
-		Passenger s6p4 = new Passenger("Oskars Muthoni",610340,4d,95,pregnancy,2023-04-01 12:45:00);
-		Passenger s6p5 = new Passenger("Anastasia Amahle",950562,2a,350,kid,2023-04-01 13:30:00);
-		Passenger s6p6 = new Passenger("Yasen Alaric",396014,1b,95,physical disability,2023-04-01 15:00:00);
-		Passenger s6p7 = new Passenger("Rozalija Raginaharjaz",1130584,2c,125,pregnancy,2023-04-01 16:15:00);
-		Passenger s6p8 = new Passenger("Yarik Komang",851944,1d,500,mental disability,2023-04-01 18:00:00);
-    }
-	public void setUpStage7(){
-		Passenger s7p1 = new Passenger("olvir Eusebius",A100,1a,350,physical disability,2023-04-01 10:15:00);
-		Passenger s7p2 = new Passenger("Cassian Rufina",290123,2b,125,mental disability,2023-04-01 10:15:00);
-		Passenger s7p3 = new Passenger("Harpreet Aleksandra",18R200,3c,500,age,2023-04-01 11:00:00);
-		Passenger s7p4 = new Passenger("Oskars Muthoni",610+340,4d,95,pregnancy,2023-04-01 12:45:00);
-		Passenger s7p5 = new Passenger("Anastasia Amahle",950562,5e,350,kid,2023-04-01 13:30:00);
-		Passenger s7p6 = new Passenger("Yasen Alaric",396*014,6f,95,physical disability,2023-04-01 15:00:00);
-		Passenger s7p7 = new Passenger("Rozalija Raginaharjaz",1130584,7,pregnancy,2023-04-01 16:15:00);
-		Passenger s7p8 = new Passenger("Yarik Komang",851+-944,8p,125,s00,mental disability,2023-04-01 18:00:00);
-    }
-	public void setUpStage8(){
-		Passenger s8p1 = new Passenger("olvir Eusebius",1110,1a,350,physical disability,2023-04-01 08:30:00);
-    }
-	public void setUpStage9(){
-		Passenger s9p1 = new Passenger("anatoly karpov",1110290182,4a,908,none,2023-04-01 08:35:00);
-    }
-	
-	
-	public void HashInserttest1(){
-	   setUpStage4();
-	   setUpStage8();
+	public void uploadtest1(){
+	   AirlineController arc= new AirlineController();
 	   try{
-		   HashTable has= new HashTable(1);
-		   has.insert(s4p1.getticket(),s4p1);
-		   has.insert(s4p2.getticket(),s4p2);
-		   has.insert(s4p3.getticket(),s4p3);
-		   has.insert(s4p4.getticket(),s4p4);
-		   has.insert(s4p5.getticket(),s4p5);
-		   has.insert(s4p6.getticket(),s4p6);
-		   has.insert(s4p7.getticket(),s4p7);
-		   has.insert(s4p8.getticket(),s4p8);
-		   has.insert(s4p9.getticket(),s4p9);
-		   has.insert(s4p10.getticket(),s4p10);
-		   has.insert(s8p1.getticket(),s8p1);
-		   assertEquals(11,has.getsize());
+		  arc.jsonInFlightPassengerInfo("D:\Documents\Downloads\a\airline_administrator\src\outs_or_inputs\passengerInfoSetupStage6"); 
+		  arc.jsonInFlightInfo("D:\Documents\Downloads\a\airline_administrator\src\outs_or_inputs\flightInfoSetupStage1");
+		  assertequals(0,0);
 	   }catch (Exception e){
 		   fail();
 	   }
     }
-	public void HashInserttest2(){
-        setUpStage6();
-		setUpStage8();
-		try{
-		   HashTable has= new HashTable(1);
-		   has.insert(s6p1.getticket(),s6p1);
-		   has.insert(s6p2.getticket(),s6p2);
-		   has.insert(s6p3.getticket(),s6p3);
-		   has.insert(s6p4.getticket(),s6p4);
-		   has.insert(s6p5.getticket(),s6p5);
-		   has.insert(s6p6.getticket(),s6p6);
-		   has.insert(s6p7.getticket(),s6p7);
-		   has.insert(s6p8.getticket(),s6p8);
-		   has.insert(s8p1.getticket(),s8p1);
+	public void uploadtest2(){
+	   AirlineController arc= new AirlineController();
+	   try{
+		  arc.jsonInFlightPassengerInfo("D:\Documents\Downloads\a\airline_administrator\src\outs_or_inputs\passengerInfoSetupStage4"); 
+		  arc.jsonInFlightInfo("D:\Documents\Downloads\a\airline_administrator\src\outs_or_inputs\flightInfoSetupStage1");
+		  assertequals(0,0);
+	   }catch (Exception e){
 		   fail();
-		}catch(DuplicatedObjectExeption e){
-			assertNotNull(e);
-		}
+	   }
     }
-	public void HashInserttest3(){
-      setUpStage1();
-	  try{
-		HashTable has= new HashTable(1);
-		has.insert(fly1.getnumflight(),fly1);
-		fail();
-	  }catch(IncorrectObjectExeption e){
-		 assertNotNull(e); 
-	  }
+	public void uploadtest3(){
+	   AirlineController arc= new AirlineController();
+	   try{
+		  arc.jsonInFlightPassengerInfo("D:\Documents\Downloads\a\airline_administrator\src\outs_or_inputs\passengerInfoSetupStage4"); 
+		  arc.jsonInFlightInfo("D:\Documents\Downloads\a\airline_administrator\src\outs_or_inputs\flightInfoSetupStage3");
+		  fail();
+	   }catch (SizePlaneExeption e){
+		   assertNotnull(e);
+	   }
     }
-	
+	public void uploadtest4(){
+	   AirlineController arc= new AirlineController();
+	   try{
+		  arc.jsonInFlightPassengerInfo("D:\Documents\Downloads\a\airline_administrator\src\outs_or_inputs\passengerInfoSetupStage4"); 
+		  fail();
+	   }catch (MissingPlaneDataExeption e){
+		   assertNotnull(e);
+	   }
+    }
+	public void uploadtest5(){
+	   AirlineController arc= new AirlineController();
+	   try{
+		  arc.jsonInFlightInfo("D:\Documents\Downloads\a\airline_administrator\src\outs_or_inputs\flightInfoSetupStage1");
+		  fail();
+	   }catch (MissingPassengerDataExeption e){
+		   assertNotnull(e);
+	   }
+    }
 }
