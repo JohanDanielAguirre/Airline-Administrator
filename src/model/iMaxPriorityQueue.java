9c6d80210ecd82a059ffc116e52fb79a38d800cb
SebasL07 <100957510+SebasL07@users.noreply.github.com>
2023-04-30 22:11:28 -0500

    minimum change

--- src/model/iMaxPriorityQueue.java
+++ src/model/iMaxPriorityQueue.java
@@ -6,7 +6,7 @@ import Exeptions.SizePlaneExeption;
 
 public interface iMaxPriorityQueue {
     public void insert(Passenger passenger) throws SizePlaneExeption, KeyExeption, NonexistObjectExeption;    
-    public Passenger Maximum();
+    public Passenger Maximum() throws Exception;
     public Passenger extract_Max();
-    public void increase_Key(int x, int newKey);
+    public void increase_Key(int x, int newKey) throws KeyExeption, SizePlaneExeption, NonexistObjectExeption;
 }
